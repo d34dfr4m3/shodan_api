e32de6088c58f04eaf05985034142692549ff1c6
d34dfr4m3 <32407502+d34dfr4m3@users.noreply.github.com>
2017-12-30 11:04:50 -0200

    Create shodan_api.py

---
+++ shodan_api.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import json, shodan, sys
+
+key='[CENSURADO]'
+api=shodan.Shodan(key)
+def search():
+	if len(sys.argv) < 1:
+		what=input("Search: ")
+	else:
+		what=sys.argv[1]
+	try:
+		results = api.search(what)
+		for result in results['matches']:
+			print('------------------------')
+			'''
+			print('IP: %s' % result['ip_str'], ' Hostname:', result['hostnames'] )
+			print('OS: ', result['os'])
+			print(' ')
+			'''
+			hostsearch(result['ip_str'])
+		print(results['total'])
+	except Exception as error:
+		print("Error: ", error)
+def hostsearch(target):
+	host =	api.host(target)
+	print('''IP: %s 
+		Organization: %s
+		OS:  %s ''' % (host['ip_str'],host.get('org','n/a'), host.get('os', 'n/a' )))
+	
+def banners(target):
+	#https://shodan.readthedocs.io/en/latest/tutorial.html
+	pass
+def main():
+	print("Come on") 
+	search()
+main()
