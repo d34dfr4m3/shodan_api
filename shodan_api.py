884095ae2b94c1eb64c9c0a3103ca0694bf45d93
d34dfr4m3 <32407502+d34dfr4m3@users.noreply.github.com>
2017-12-31 00:37:17 -0200

    Update shodan_api.py

--- shodan_api.py
+++ shodan_api.py
@@ -3,34 +3,33 @@ import json, shodan, sys
 
 key='[CENSURADO]'
 api=shodan.Shodan(key)
+
 def search():
-	if len(sys.argv) < 1:
-		what=input("Search: ")
-	else:
-		what=sys.argv[1]
-	try:
-		results = api.search(what)
-		for result in results['matches']:
-			print('------------------------')
-			'''
-			print('IP: %s' % result['ip_str'], ' Hostname:', result['hostnames'] )
-			print('OS: ', result['os'])
-			print(' ')
-			'''
-			hostsearch(result['ip_str'])
-		print(results['total'])
-	except Exception as error:
-		print("Error: ", error)
+    if len(sys.argv) < 1:
+        what=input("Search: ")
+    else:
+        what=sys.argv[1]
+    try:
+        results = api.search(what)
+        for result in results['matches']:
+            print('------------------------')
+            '''
+            print('IP: %s' % result['ip_str'], ' Hostname:', result['hostnames'] )
+            print('OS: ', result['os'])
+            print(' ')
+            '''
+            hostsearch(result['ip_str'])
+        print(results['total'])
+    except Exception as error:
+        print("Error: ", error)
 def hostsearch(target):
-	host =	api.host(target)
-	print('''IP: %s 
-		Organization: %s
-		OS:  %s ''' % (host['ip_str'],host.get('org','n/a'), host.get('os', 'n/a' )))
-	
-def banners(target):
-	#https://shodan.readthedocs.io/en/latest/tutorial.html
-	pass
+    host =  api.host(target)
+    print('''IP: %s
+                Organization: %s
+                OS:  %s ''' % (host['ip_str'],host.get('org','n/a'), host.get('os', 'n/a' ))) 
+    for item in host['data']:
+        print("Port: "+str(item['port'])+"\nBanner :"+str(item['data']))
 def main():
-	print("Come on") 
-	search()
+    print("Come on") 
+    search()
 main()
